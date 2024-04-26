5df715bf455e6a99604007b560408a72601383c0
HaritsTama <nayotama1818@gmail.com>
2024-04-26 20:54:31 +0700

    memasukan node untuk nim

--- 141_singlelinkedlist/141_singlelinkedlist.cpp
+++ 141_singlelinkedlist/141_singlelinkedlist.cpp
@@ -14,4 +14,11 @@ void addNode()
 {
 	int nim;
 	string nama;
+	Node* nodeBaru = new Node();
+	cout << "Masukan NIM = ";
+	cin >> nim;
+	cout << "Masukan Nama = ";
+	cin >> nama;
+	nodeBaru->noMhs = nim;
+	
 }
